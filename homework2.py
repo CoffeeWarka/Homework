4be97ed5f48467ed00b3414b664b37f7f2923abb
CoffeeWarka <erleronphoenix@gmail.com>
2024-06-28 22:18:28 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_count = 12
+homework_time = 1.5
+course = 'Python'
+average_time_homework = homework_time/homework_count
+print('Курс :',course,', всего задач : ',homework_count,', затрачено часов :',homework_time,
+      ', среднее время выполнения',average_time_homework,'часа.')
