25872cca4ec8e92865daff0e41c1a18ab7c92021
CoffeeWarka <erleronphoenix@gmail.com>
2024-07-24 19:50:07 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,9 +1,9 @@
-from fake_math import divide_zero_not
-from true_math import divide_zero
-result1 = divide_zero_not(48, 0)
-result2 = divide_zero_not(6, 1)
-result3 = divide_zero(20, 0)
-result4 = divide_zero(17, 117)
+from fake_math import divide_zero as not_dz
+from true_math import divide_zero as dz
+result1 = not_dz(48, 0)
+result2 = not_dz(6, 1)
+result3 = dz(20, 0)
+result4 = dz(17, 117)
 print(result1)
 print(result2)
 print(result3)
