41f414c91d7a9b50e4e4bfc7bdd261564487a6fa
CoffeeWarka <erleronphoenix@gmail.com>
2024-08-12 12:41:15 +0300

    Update worktest.py

--- worktest.py
+++ worktest.py
@@ -2,6 +2,7 @@ import time
 
 
 class User:
+
     def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = password
@@ -22,15 +23,18 @@ class UrTube:
         self.videos = videos
         self.current_user = current_user
 
+
+
     user_log_in = False
     # good_one
     def log_in(self, nickname, password):
         for i in self.users:
-            # hash_pass = i.password
-            if i.__contains__(nickname) and i.__contains__(password):
+            hash_user_pass = hash(self.password)
+            hash_log_in_pass = hash(password)
+            if nickname in self.nickname and hash_user_pass.__eq__(hash_log_in_pass):
                 self.current_user = User(self.nickname, self.password, self.age)
                 UrTube.user_log_in = True
-            return UrTube.user_log_in
+                return UrTube.user_log_in
 
 
     def register(self, nickname, password, age):
@@ -38,16 +42,19 @@ class UrTube:
         self.password = password
         self.age = age
         users = self.users
-        for i in users:
-            if i.__contains__(nickname):
-                print(f'Пользователь {nickname} уже существует.')
-        else:
-                users = users.append([nickname, hash(password), age])
-                UrTube.log_in(self, nickname, hash(password))
+        UrTube.log_in(self, nickname, password)
+        if UrTube.user_log_in == False:
+            for i in users:
+                if i.nickname.__contains__(nickname):
+                    print(f'Пользователь {nickname} уже существует.')
+            else:
+                users = users.append(User)
+                UrTube.log_in(self, nickname, password)
+
 
     def log_out(self):
         self.current_user = None
-
+        return self.current_user
     def add(self, *args):
         videos_list = self.videos
         vid = Video
@@ -69,7 +76,7 @@ class UrTube:
             print('Войдите в аккаунт, чтобы смотреть видео')
         elif self.age < 18:
             print("Вам нет 18 лет, пожалуйста покиньте страницу")
-        else:
+        elif UrTube.user_log_in:
             for i in self.videos:
                 if video_title.__eq__(i.title):
                     j = 1
@@ -78,26 +85,22 @@ class UrTube:
                         time.sleep(1)
                         j += 1
                     print('Конец видео')
-                else:
-                    str(self.current_user)
 
-    def __str__(self):
-        return f'{self.current_user}'
 
         #good_one
 
 
 
 
-
-if __name__ == 'main':
-    user = User()
-    video = Video()
+#
+# if __name__ == 'main':
+#     user = User()
+#     video = Video()
 
 
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+v2 = Video('Для чего девушкам парень программист?', 1, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
