546cc078de176daac89eb3c3ce253067342be955
CoffeeWarka <erleronphoenix@gmail.com>
2024-08-15 16:46:16 +0300

    Update module6hard.py

--- module6hard.py
+++ module6hard.py
@@ -1,12 +1,12 @@
 class Figure:
-    sides_count = 0
+    sides_count = 2
 
     def __init__(self, __color=[], __sides=[], filled=False):
         self.__sides = __sides
         self.__color = __color
         self.filled = filled
 
-    def get_collor(self):
+    def get_color(self):
         color_list = list(self.__color)
         return color_list
 
@@ -27,10 +27,45 @@ class Figure:
         self.b = b
         self.__is_valid_color(r,g,b)
 
+    def __is_valid_sides(self, *args):
+        self.args = args
+        for i in args:
+            if i >= 0 and len(args) == self.sides_count:
+                return True
+            else:
+                return False
+
+    def set_sides(self, *new_sides):
+        self.new_sides = new_sides
+        if self.__is_valid_sides(*new_sides):
+            if len(new_sides) == self.sides_count:
+                self.__sides = self.new_sides
+            else:
+                self.__sides = self.__sides
+
+    def get_sides(self):
+        sides = self.__sides
+        return sides
 
 
 
 
-fig = Figure((200,200,200), 10)
-fig.set_color(44, 33, 55)
-print(fig.get_collor())
+
+
+fig = Figure((200,200,200), 5)
+# fig.set_color(44, 33, 55)
+# print(fig.get_color())
+# fig.set_sides(5,3,12,4,5)
+# print(fig.get_sides())
+# print(fig.is_valid_sides(11))
+fig.set_sides(125, 111)
+print(fig.get_sides())
+
+создание массива с одинаковыми значениями в питоне
+import numpy as np
+np.full(
+  shape=10,
+  fill_value=3,
+  dtype=np.int
+)
+> array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
