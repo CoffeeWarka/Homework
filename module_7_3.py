b2d22f2227078ee56f541ce6956e51f96149e3eb
CoffeeWarka <erleronphoenix@gmail.com>
2024-08-21 12:36:22 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -7,8 +7,8 @@ class WordsFinder():
 
     def get_all_words(self):
         all_words = {}
-        strip_words = []
         for i in self.args:
+            strip_words = []
             with open(i, encoding='utf-8') as file:
                 for j in file:
                     j_low = j.lower()
@@ -16,25 +16,29 @@ class WordsFinder():
                     for p in punctuation:
                         if p in j_low:
                             j_low = j_low.replace(p,'')
-                            strip_words += j_low.split()
-                    all_words[f"{i}"] = strip_words
-                return all_words
+                    strip_words += j_low.split()
+            all_words[f"{i}"] = strip_words
+        return all_words
 
     def find(self, word):
         match_dict = {}
         for name, words in self.get_all_words().items():
+            index = 0
             for i in words:
+                index +=1
+
+
                 if i == word.lower():
-                    match_dict[f'{name}'] = (words.index(i)) + 1
-                    return match_dict
+                    match_dict[f'{name}'] = index
+                    break
+        return match_dict
 
     def count(self, word):
         count_dict = {}
-        count = 0
         for name, words in self.get_all_words().items():
+            count = 0
             for i in words:
                 if i == word.lower():
                     count += 1
             count_dict[f'{name}'] = count
-            return count_dict
-
+        return count_dict
