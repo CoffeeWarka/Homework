f21389b2b14c5451b6d01e6b449034da2dd6d085
CoffeeWarka <erleronphoenix@gmail.com>
2024-08-26 10:32:50 +0300

    Update test 2.py

--- test 2.py
+++ test 2.py
@@ -22,3 +22,17 @@ print('Волшебники данных решили задачи за {} с !'
 print(f'Команды решили {score_1} и {score_2} задач.')
 print(f'Результат битвы: {challenge_result}')
 print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
+
+import os
+import time
+
+directory = os.getcwd()
+for root, dirs, files in os.walk(directory):
+for file in files:
+filepath = os.path.join(directory, file)
+filetime = os.path.getmtime(file)
+formatted_time = time.strftime('%d.%m.%Y. %H.%M', time.localtime(filetime))
+filesize = os.path.getsize(file)
+parent_dir = os.path.dirname(directory)
+print(f'Обнаружен файл: {file}, Путь: {filepath}, '
+f'Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
