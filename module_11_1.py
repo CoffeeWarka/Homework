50193f75d1268dd916c66863734d5093c58d2c90
CoffeeWarka <erleronphoenix@gmail.com>
2024-10-09 16:36:22 +0300

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -1,20 +1,24 @@
 import requests
 import pandas
 
-# pandas - file check
 # numpy - massive
 # matplotlib - visual data
 # pillow - images
 
 # requests
+
+# vk_head = requests.get('https://vk.com')
+# print(vk_head.headers)
+
+
 # def ya_search(search):
 #     query = {'q': search}
 #     req = requests.get('https://ya.ru/?utm_referrer=https%3A%2F%2Fyandex.ru%2F', params = query)
 #     print(req.url)
 #
 #
-# def alive(site_adress):
-#     response = requests.get(site_adress)
+# def alive(site_address):
+#     response = requests.get(site_address)
 #     if response.status_code == 200:
 #         print("It's Alive!")
 #     elif response.status_code == 404:
@@ -30,26 +34,26 @@ import pandas
 #
 #
 #
-sheet = {'Продукт':['Горбуша','Свинина', 'Курица', 'Куриное яйцо'], 'Ккал':[151, 484, 161, 153]}
-df = pandas.DataFrame(sheet)
+# sheet = {'Продукт':['Горбуша','Свинина', 'Курица', 'Куриное яйцо'], 'Ккал':[151, 484, 161, 153]}
+# df = pandas.DataFrame(sheet)
 # print(df)
 # print(df.describe())
-# print(df.sort_values('Ккал', ascending=False).head())
+# print(df.sort_values('Ккал', ascending=True).head())
 
-def sheet_plus(sheet_concat, products, kcals):
-    kcals_index = 0
-    if isinstance(kcals, int) and isinstance(products, str):
-        products = [products]
-        kcals = [kcals]
-    for product in products:
-        kcal = kcals[kcals_index]
-        new_product = {'Продукт': product, 'Ккал': kcal}
-        df_plus = pandas.DataFrame([new_product])
-        sheet_concat = pandas.concat([sheet_concat, df_plus], ignore_index=True)
-        kcals_index+=1
-    return sheet_concat
-
-
-
-sp = sheet_plus(df,'Банан', 98)
-print(sp)
+# def sheet_plus(sheet_concat, products, kcals):
+#     kcals_index = 0
+#     if isinstance(kcals, int) and isinstance(products, str):
+#         products = [products]
+#         kcals = [kcals]
+#     for product in products:
+#         kcal = kcals[kcals_index]
+#         new_product = {'Продукт': product, 'Ккал': kcal}
+#         df_plus = pandas.DataFrame([new_product])
+#         sheet_concat = pandas.concat([sheet_concat, df_plus], ignore_index=True)
+#         kcals_index+=1
+#     return sheet_concat
+#
+#
+#
+# sp = sheet_plus(df,'Банан', 98)
+# print(sp)
