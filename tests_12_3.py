89ff3ff44dd043219f6cdb2fe8ee4244a40c677d
CoffeeWarka <erleronphoenix@gmail.com>
2024-10-19 12:37:47 +0300

    Update tests_12_3.py

--- tests_12_3.py
+++ tests_12_3.py
@@ -110,20 +110,20 @@ class TournamentTest(unittest.TestCase):
         turn_1 = Tournament(90, self.runner_1, self.runner_3)
         result = turn_1.start()
         # print(result[list(result.keys())[-1]] == 'Ник')
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_turn1'] = result
 
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_turn2(self):
         turn_2 = Tournament(90, self.runner_2, self.runner_3)
         result = turn_2.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_turn2'] = result
 
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_turn3(self):
         turn_3 = Tournament(90, self.runner_1, self.runner_2, self.runner_3)
         result = turn_3.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_turn3'] = result
 
