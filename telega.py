c2a2be561e0747df0a3959d228bd276c85caad09
CoffeeWarka <erleronphoenix@gmail.com>
2024-10-25 10:15:43 +0300

    Add files via upload

---
+++ telega.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm.storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
