10de7ee2e1d169826295aa6b0eb4dc18b95bd7a3
CoffeeWarka <erleronphoenix@gmail.com>
2024-10-27 14:57:08 +0300

    Update and rename telega3.py to module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -2,13 +2,16 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 
 api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 class UserState(StatesGroup):
-    age, growth, weight = State()
+    age = State()
+    growth = State()
+    weight = State()
 
 @dp.message_handler(text='Calories')
 async def set_age(message):
@@ -17,7 +20,7 @@ async def set_age(message):
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update_data(growth=message.text)
+    await state.update_data(age=message.text)
     await message.answer('Введите свой рост:')
     await UserState.growth.set()
 
@@ -25,14 +28,17 @@ async def set_growth(message, state):
 async def set_weight(message, state):
     await state.update_data(growth=message.text)
     await message.answer('Введите свой вес:')
-    await UserState.growth.set()
+    await UserState.weight.set()
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
     await state.update_data(weight=message.text)
     data = await state.get_data()
-    calories_calc = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
-    await message.answer(f'Ваша норма калорий {calories_calc}')
+    data_weight = int(data['weight'])
+    data_growth = int(data['growth'])
+    data_age = int(data['age'])
+    calories_calc = 10 * data_weight + 6.25 * data_growth - 5 * data_age + 5
+    await message.answer(f"Ваша норма калорий {calories_calc}")
     await state.finish()
 
 
