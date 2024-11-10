f00cf976f85a9d28b33680376bdf5ecbbc16c5bf
CoffeeWarka <erleronphoenix@gmail.com>
2024-11-10 23:53:29 +0300

    Update crud_function.py

--- crud_function.py
+++ crud_function.py
@@ -5,15 +5,22 @@ connection = sqlite3.connect('crud_db.db')
 cursor = connection.cursor()
 
 
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products(
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-price INTEGER NOT NULL
-);
-''')
+
+def get_all_products():
+    cursor.execute('SELECT title, description, price FROM Products')
+    prod_list = cursor.fetchall()
+    return prod_list
+
 cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
                ('Ускорин', 'Теряй калории быстро', 100))
 cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
@@ -23,9 +30,4 @@ cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?
 cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
                ('Влюбин', 'Полюби заниматься спортом', 400))
 connection.commit()
-def get_all_products():
-    cursor.execute('SELECT title, description, price FROM Products')
-    prod_list = cursor.fetchall()
-    connection.commit()
-    connection.close()
-    return prod_list
+
